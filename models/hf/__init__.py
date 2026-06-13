9d4113bc712e37a5394a89246b6d49b82d94e9e2
Rouy Alfahel <130895594+Royalfahel@users.noreply.github.com>
2026-06-13 11:00:04 +0200

    Initialize models.hf with classifier imports

---
+++ models/hf/__init__.py
@@ -0,0 +1,9 @@
+from .bert import ParticleBertClassifier
+from .roberta import ParticleRobertaClassifier
+from .mamba import ParticleMambaClassifier
+
+__all__ = [
+    "ParticleBertClassifier",
+    "ParticleRobertaClassifier",
+    "ParticleMambaClassifier",
+]
