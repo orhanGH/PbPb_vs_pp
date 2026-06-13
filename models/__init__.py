5bf5c76a29853d33f4506bb74ae7f36f7f081485
Rouy Alfahel <130895594+Royalfahel@users.noreply.github.com>
2026-06-13 10:52:24 +0200

    Refactor imports and update __all__ in init.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,12 @@
-from .efn import EFN, build_mlp
-from .oefn import ObservableEFN
-from .mefn import MomentEFN
-from .aefn import AttentionEFN
+from .efn import EFN, ObservableEFN, MomentEFN, AttentionEFN
+from .hf import ParticleBertClassifier, ParticleRobertaClassifier, ParticleMambaClassifier
 
 __all__ = [
     "EFN",
     "ObservableEFN",
     "MomentEFN",
     "AttentionEFN",
-    "build_mlp",
+    "ParticleBertClassifier",
+    "ParticleRobertaClassifier",
+    "ParticleMambaClassifier",
 ]
